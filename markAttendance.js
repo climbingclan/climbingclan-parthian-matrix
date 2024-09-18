f27a205e5043a47139fa286cbe630450e46c57e8
Tim Dobson <hello@tdobson.net>
2024-09-18 22:32:43 +0100

    refactor: replace var with let/const in markX.js files

--- markAttendance.js
+++ markAttendance.js
@@ -1,104 +1,99 @@
 
 function markAttended(){
+  const attendancetype = "Attended"
+  const attendanceshow = "Attended"
+  const metakey = "cc_attendance"
+  const orderstatus= "completed"
+  const metavalue = "attended"
 
-var attendancetype = "Attended"
-var attendanceshow = "Attended"
-var metakey = "cc_attendance"
-var orderstatus= "completed"
-var metavalue = "attended"
-
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markCancelled(){
+  const attendancetype = "Cancel"
+  const attendanceshow = "Cancelled"
+  const metakey = "cc_attendance"
+  const orderstatus= "completed"
+  const metavalue = "cancelled"
 
-var attendancetype = "Cancel"
-var attendanceshow = "Cancelled"
-var metakey = "cc_attendance"
-var orderstatus= "completed"
-var metavalue = "cancelled"
-
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markNoShow(){
+  const attendancetype = "NoShow"
+  const attendanceshow = "NoShow"
+  const orderstatus= "completed"
+  const metakey = "cc_attendance"
+  const metavalue = "noshow"
 
-var attendancetype = "NoShow"
-var attendanceshow = "NoShow"
-var orderstatus= "completed"
-var metakey = "cc_attendance"
-var metavalue = "noshow"
-
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markDuplicate(){
-var attendancetype = "Duplicate"
-var attendanceshow = "Duplicated"
-var orderstatus= "completed"
-var metakey = "cc_attendance"
-var metavalue = "duplicate"
+  const attendancetype = "Duplicate"
+  const attendanceshow = "Duplicated"
+  const orderstatus= "completed"
+  const metakey = "cc_attendance"
+  const metavalue = "duplicate"
 
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markLateBail(){
-var attendancetype = "Late Bail"
-var attendanceshow = "Late Bail"
-var orderstatus= "completed"
-var metakey = "cc_attendance"
-var metavalue = "late-bail"
+  const attendancetype = "Late Bail"
+  const attendanceshow = "Late Bail"
+  const orderstatus= "completed"
+  const metakey = "cc_attendance"
+  const metavalue = "late-bail"
 
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markNoRegisterShow(){
-var attendancetype = "No Register Show"
-var attendanceshow = "No Register Show"
-var orderstatus= "completed"
-var metakey = "cc_attendance"
-var metavalue = "noregistershow"
+  const attendancetype = "No Register Show"
+  const attendanceshow = "No Register Show"
+  const orderstatus= "completed"
+  const metakey = "cc_attendance"
+  const metavalue = "noregistershow"
 
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markVolunteerLateBail(){
-var attendancetype = "Late Bail"
-var attendanceshow = "Late Bail"
-var metakey = "cc_attendance"
-var metavalue = "late-bail"
+  const attendancetype = "Late Bail"
+  const attendanceshow = "Late Bail"
+  const metakey = "cc_attendance"
+  const metavalue = "late-bail"
 
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markVolunteerCancelled(){
+  const attendancetype = "Cancel"
+  const attendanceshow = "Cancelled"
+  const metakey = "cc_attendance"
+  const orderstatus= "completed"
+  const metavalue = "cancelled"
 
-var attendancetype = "Cancel"
-var attendanceshow = "Cancelled"
-var metakey = "cc_attendance"
-var orderstatus= "completed"
-var metavalue = "cancelled"
-
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markVolunteerNoShow(){
+  const attendancetype = "NoShow"
+  const attendanceshow = "NoShow"
+  const orderstatus= "completed"
+  const metakey = "cc_attendance"
+  const metavalue = "noshow"
 
-var attendancetype = "NoShow"
-var attendanceshow = "NoShow"
-var orderstatus= "completed"
-var metakey = "cc_attendance"
-var metavalue = "noshow"
-
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markVolunteerAttended(){
-var attendancetype = "Attended"
-var orderstatus= "completed"
-var metavalue = "attended"
+  const attendancetype = "Attended"
+  const orderstatus= "completed"
+  const metavalue = "attended"
 
-markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
+  markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue)
 }
 
 function markAttendance(attendancetype, attendanceshow, orderstatus, metakey, metavalue ) {
