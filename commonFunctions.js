3e3bf013718822f31d759792e1fc12fe84e05286
Tim Dobson <hello@tdobson.net>
2024-09-18 13:29:58 +0100

    feat: Define cell as a constant in commonFunctions.js

--- commonFunctions.js
+++ commonFunctions.js
@@ -9,6 +9,8 @@ var apidomain="climbingclan.com"
 var apiusername="ck_3f8cd172e7aed36533d434e04e8c0b2affe19075"
 var apipassword="cs_817f3cd22ae28bc33fa716a6fdfd707188c0409b"
 
+const cell = setupCell("Dashboard", "B5");
+
 
 const colors = {
   lightRed: "#ffcccb",
@@ -140,7 +142,6 @@ function setNumberFormat(sheet, columnHeader, format) {
 }
 
 function makeReport(stmt, reportConfig) {
-  let cell = setupCell("Dashboard", "B5");
   console.log("Cell value in makeReport:", cell);
   let sheet = setupSheet(reportConfig.sheetName);
 
