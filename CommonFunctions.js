3534a23f2d872eaf563f117a364d2eaee36c0ff3
Tim Dobson <hello@tdobson.net>
2024-09-18 12:47:14 +0100

    refactor: move functions from Code.js to CommonFunctions.js

--- CommonFunctions.js
+++ CommonFunctions.js
@@ -174,6 +174,79 @@ function refreshOutput() {
   conn.close();
 }
 
+function getIP() {
+  var url = "http://api.ipify.org";
+  var json = UrlFetchApp.fetch(url);
+  Logger.log(json);
+}
+
+function appendToSheetWithNoClear(sheet, results) {
+  let metaData = results.getMetaData();
+  let numCols = metaData.getColumnCount();
+  const rows = [];
+
+  // First row with column labels
+  const colLabels = [];
+  for (let col = 0; col < numCols; col++) {
+    colLabels.push(metaData.getColumnLabel(col + 1));
+  }
+  rows.push(colLabels);
+
+  // Remaining rows with results
+  while (results.next()) {
+    const row = [];
+    for (let col = 0; col < numCols; col++) {
+      row.push(results.getString(col + 1));
+    }
+    rows.push(row);
+  }
+
+  // Find the last row containing a value
+  const lastRow = sheet.getDataRange().getLastRow();
+
+  // Set the values of the rows starting from the row below the last row containing a value
+  // or the top row if it is empty
+  let startRow = lastRow + 1;
+  const topRowValues = sheet.getRange(1, 1, 1, numCols).getValues();
+  if (topRowValues[0].every(value => value === "")) {
+    startRow = 1;
+  }
+
+  sheet.getRange(startRow, 1, rows.length, numCols).setValues(rows);
+  // Set the font size of the rows with column labels to 14
+  sheet.getRange(1, 1, 1, numCols).setFontSize(14);
+  sheet.autoResizeColumns(1, numCols + 1);
+}
+
+function searchEmailsSheet(ccVolunteerOld) {
+  var sheetName = 'Emails';
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+  var dataRange = sheet.getDataRange();
+  var values = dataRange.getValues();
+  var numRows = dataRange.getNumRows();
+  var numCols = dataRange.getNumColumns();
+  var returnObj = {};
+  for (var i = 1; i < numRows; i++) {
+    if (values[i][0] == 'cc_volunteer_old') {
+      var foundColumn = -1;
+      for (var j = 1; j < numCols; j++) {
+        if (values[i][j] == ccVolunteerOld) {
+          foundColumn = j;
+          break;
+        }
+      }
+      if (foundColumn == -1) {
+        throw new Error('Column not found');
+      }
+      for (var k = 1; k < numRows; k++) {
+        returnObj[values[k][0]] = values[k][foundColumn];
+      }
+      break;
+    }
+  }
+  return returnObj;
+}
+
 function refreshStats() {
   var conn = Jdbc.getConnection(url, username, password);
   var stmt = conn.createStatement();
